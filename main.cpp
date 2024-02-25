f8d598f00f3134c6d20c237914ad8d15f2b2dec3
xotnet <150250186+xotnet@users.noreply.github.com>
2024-02-25 07:18:30 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,23 +7,14 @@ int main(int argc, char** argv) {
 		std::string key = argv[2];
 		std::string cryptedBin = "";
 		std::string preBin = "";
-		for (char c : message) {
+		for (char c : message) { // every char in message
 			std::bitset<8> bits(c); // bitset is container with size 8
-			preBin += bits.to_string();
-		}
-		for (int i = 0; i < preBin.length(); i++) { // every bit of message
-			bool thisBitOfMessage, eqBitOfMessage = 0;
-			if (preBin[i] == '1') {thisBitOfMessage = 1; eqBitOfMessage = 1;} else {thisBitOfMessage = 0; eqBitOfMessage = 0;}
-			for (int u = 0; u < key.length(); u++) { // every char of key
+			for (int u = 0; u < key.length(); u++) { // bitset in key char
 				std::bitset<8> keyCharToBit(key[u]);
-				for (int m = 0; m < 8; m++) { // every bit of key's symbol
-					if (keyCharToBit[m] != eqBitOfMessage) {
-						if (thisBitOfMessage == 1) {thisBitOfMessage = 0;} 
-						else if (thisBitOfMessage == 0) {thisBitOfMessage = 1;}
-					}
-				}
+				bits = bits ^ keyCharToBit;
+				bits = bits & keyCharToBit;
 			}
-			cryptedBin = cryptedBin + std::to_string(thisBitOfMessage);
+			cryptedBin = cryptedBin + bits.to_string();
 		}
 		std::cout << "Encrypted bits: " << cryptedBin;
 	} else {
